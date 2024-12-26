f9a6c178c5c831a3c1981f43411a9ae06cc47d46
ankita8412 <ankitamane816@gmail.com>
2024-12-26 12:07:54 +0530

    crypto setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: '' ,
+    pathMatch: "full",
+  },
+  {
+    path: "",
+    loadChildren: () =>
+      import("../app/components/auth/auth.module").then((m) => m.AuthModule),
+  },
+  {
+    path: "admin",
+    loadChildren: () =>
+      import("../app/components/admin/admin.module").then((m) => m.AdminModule),
+  },
+  {
+    path: "trader",
+    loadChildren: () =>
+      import("../app/components/trader/trader.module").then((m) => m.TraderModule),
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,79 @@
-import { Component } from '@angular/core';
-
+import { AfterContentChecked, Component, OnInit } from '@angular/core';
+import { SharedService } from './shared/shared.service';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'crypto-app';
+export class AppComponent implements OnInit,AfterContentChecked{
+  title = 'Crypto';
+  data: any = {} ;
+  isLoading = false;
+  isLoading1 = false;
+  isTrader = false;
+  isAdminDashboard = false;
+
+  constructor(private _sharedService: SharedService,private router: Router,){}
+  
+  ngOnInit() {
+    this._sharedService.isLoading$.subscribe({
+      next: (res: any) => {
+        if (res) {
+          this.isLoading = res;
+        } else {
+          this.isLoading = res
+        }
+      }
+    })
+    this._sharedService.isLoading1$.subscribe({
+      next: (res: any) => {
+        if (res) {
+          this.isLoading1 = res;
+        } else {
+          this.isLoading1 = res
+        }
+      }
+    })
+  }
+
+  ngAfterContentChecked(): void {
+    let currentRoute = this.router.routerState.snapshot.url;
+    if (currentRoute == "/" || currentRoute == "/auth" || currentRoute == "/auth/sign-up") {
+      this.isAdminDashboard = false;
+      this.isTrader = false;
+    }else if (currentRoute?.split('/')[1] == 'admin') {
+      this.isAdminDashboard = true;
+      this.isTrader = false;
+    }
+    else if (currentRoute?.split('/')[1] == 'trader') {
+      this.isTrader = true;
+      this.isAdminDashboard = false;
+    }
+    let userData: any = localStorage.getItem('data');
+    if (JSON.parse(userData)) {
+      this.data = JSON.parse(userData);
+    }
+  }
+
+  logout() {
+      Swal.fire({
+        title: 'Are you sure?',
+        text: 'Do you want to logout',
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, Logout!',
+      }).then((result: any) => {
+        if (result.isConfirmed) {
+          this.logoutUser();
+        }
+      });
+    }
+  logoutUser() {
+    localStorage.clear();
+    window.location.href = '/auth'; 
+  }
 }
